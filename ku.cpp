81a0c86713de73123ef4e479c6e7279ac0d76023
Pantat Srikongkaew <52206979+jamepantat@users.noreply.github.com>
2019-07-02 23:03:58 +0700

    Add files via upload

---
+++ ku.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	    int TB ;
+		cout <<"Input Thai Baths : " ;
+		cin >> TB ;
+		cout << "****ExchangeRate****" << endl ;
+
+		cout << TB << " Baths is " << TB*0.033 << " dollars " << endl ;
+		cout << TB << " Baths is " << TB*0.029 << " euros " << endl ;
+		
+		return(0);  
+}
